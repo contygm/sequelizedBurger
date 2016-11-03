bbdc38f49d44e5de345a5123a4517d1c11f72103
Genevieve Conty <gmireillec@Genevieves-MacBook-Pro.local>
2016-11-03 16:07:49 -0400

    added some todos

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -9,14 +9,18 @@ router.get('/', function (req, res) {
 router.get('/burgers', function (req, res) {
 	return burger.findAll()
 	
-	.then(function(buger)){
-		res.render('index', burgObj);
+	.then(function(allBurgers)){
+		res.render('index', allBurgers);
 	}			
 	
 });
 
+// TODO: insert
 router.post('/burgers/insert', function(req, res){
-	burger.insertOne(['burger_name'], [req.body.burger_name], function(){
+	var newName = req.body.burger_name;
+
+	// TODO: find right function
+	burger.insertOne(['burger_name'], function(){
 		res.redirect('/burgers');
 	})	
 })
@@ -29,7 +33,7 @@ router.put('/burgers/update/:id', function(req, res){
 	.then(function(burger){
 		//TODO: set devoured to true
 		devoured: true;
-		
+
 		.then(function(daBurg){
 			res.redirect('/burgers');
 		})
